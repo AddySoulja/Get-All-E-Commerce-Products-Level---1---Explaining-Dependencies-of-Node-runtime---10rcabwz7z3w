1cb3dff8cc9a3525bf16e27444f9b9b023ca59a7
Aditya Kumar <adityadhanraj1357@gmail.com>
2023-05-03 18:28:21 +0530

    Build

--- src/app.js
+++ src/app.js
@@ -10,5 +10,18 @@ app.use(express.json());
 
 // Write GET endpoint for sending all the products to client here
 // Endpoint - /api/v1/products
+const api1 = async (req, res) => {
+  try {
+    // const data = await products;
+    res.status(200).json({
+      status: "success",
+      message: "Product fetched successfully",
+      products: { ...products },
+    });
+  } catch (error) {
+    res.status(404).json({ message: "Product not found", error });
+  }
+};
+app.get("/api/v1/products", api1);
 
 module.exports = app;
